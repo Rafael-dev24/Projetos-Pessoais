33d0e3da778b275d79a120be41eda126cde7c6d5
Rafael Dantas <Rd4273051@gmail.com>
2026-06-26 12:31:28 -0300

    Add script to compare two numbers and display result

---
+++ projetos/ler-dois-números-e-informar-qual-deles-e-maior.js
@@ -0,0 +1,10 @@
+console.log(" Digite dois números!");
+let num1 = Number(prompt(" Digite o primeiro número: "));
+let num2 = Number(prompt(" Digite o segundo número: "));
+
+if(num1>num2){
+    console.log(" Número 1 é maior!");
+}
+else{
+    console.log(" Número 2 é maior! ");
+}
